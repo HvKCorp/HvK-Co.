1015d442faa8e640cb290c02272a9df2d2162fc4
HvK Co <92639283+HvK-Corp@users.noreply.github.com>
2023-06-07 19:16:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 var video = document.getElementById('video-player');
-    var videoContainer = document.getElementById('video-container');
-    var siteContent = document.getElementById('site-content');
+var videoContainer = document.getElementById('video-container');
+var siteContent = document.getElementById('site-content');
+var profilePic = document.querySelector('.profile-pic');
 
-    video.addEventListener('ended', function() {
-      videoContainer.style.opacity = 0;
+video.addEventListener('ended', function() {
+  videoContainer.style.opacity = 0;
+  setTimeout(function() {
+    videoContainer.style.display = 'none';
+    siteContent.style.visibility = 'visible';
+    siteContent.style.opacity = 1;
+    animateLinks();
+    animateProfilePic();
+  }, 1000);
+});
+
+function animateLinks() {
+  var links = document.getElementsByClassName('animated-link');
+  for (var i = 0; i < links.length; i++) {
+    (function(link, index) {
       setTimeout(function() {
-        videoContainer.style.display = 'none';
-        siteContent.style.visibility = 'visible';
-        siteContent.style.opacity = 1;
-        animateLinks();
-      }, 1000);
-    });
+        link.classList.add('animated-link');
+      }, index * 200);
+    })(links[i], i);
+  }
+}
 
-    function animateLinks() {
-      var links = document.getElementsByClassName('animated-link');
-      for (var i = 0; i < links.length; i++) {
-        (function(link, index) {
-          setTimeout(function() {
-            link.classList.add('animated-link');
-          }, index * 200);
-        })(links[i], i);
-      }
-    }
+function animateProfilePic() {
+  profilePic.classList.add('animated-profile');
+}
