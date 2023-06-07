6991174fbc7d3ccdb4eb2faa1930e8281c01c0d0
HvK Co <92639283+HvK-Corp@users.noreply.github.com>
2023-06-07 18:24:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+var video = document.getElementById('video-player');
+    var videoContainer = document.getElementById('video-container');
+    var siteContent = document.getElementById('site-content');
+
+    video.addEventListener('ended', function() {
+      videoContainer.style.opacity = 0;
+      setTimeout(function() {
+        videoContainer.style.display = 'none';
+        siteContent.style.visibility = 'visible';
+        siteContent.style.opacity = 1;
+        animateLinks();
+      }, 1000);
+    });
+
+    function animateLinks() {
+      var links = document.getElementsByClassName('animated-link');
+      for (var i = 0; i < links.length; i++) {
+        (function(link, index) {
+          setTimeout(function() {
+            link.classList.add('animated-link');
+          }, index * 200);
+        })(links[i], i);
+      }
+    }
